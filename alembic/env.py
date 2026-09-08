8993256a34032ff2236ec0857d8faddcebc75e7d
Jay Hsia <jayhsia1997@gmail.com>
2026-09-08 16:34:12 -0400

    feat(migration): add 'devotion' schema to allowed schemas for migrations

--- alembic/env.py
+++ alembic/env.py
@@ -26,7 +26,7 @@ target_metadata = Base.metadata
 table_schema = settings.DATABASE_SCHEMA
 # Allowed schemas for migrations
 # NOTE: If you add a new schema, you need to add it to the ALLOWED_SCHEMAS list
-ALLOWED_SCHEMAS = {"public", "auth", "audit", "content", "app", "bible", "push"}
+ALLOWED_SCHEMAS = {"public", "auth", "audit", "content", "app", "bible", "push", "devotion"}
 
 # Get logger
 log = logging.getLogger("alembic.env")
